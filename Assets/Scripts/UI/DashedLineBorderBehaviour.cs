dcdf29a0e6de25a369b523c4191d8089ed573fa2
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-08-24 22:19:03 +0200

    Make draggable furniture and slot to both have dashed border

---
+++ Assets/Scripts/UI/DashedLineBorderBehaviour.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FFF.UI
+{
+   public class DashedLineBorderBehaviour : MonoBehaviour
+   {
+      private LineRenderer m_border;
+
+      private Image m_imgFurnitureDisplay;
+
+      public void DrawBorder()
+      {
+         m_border = GetComponent<LineRenderer>();
+
+         m_imgFurnitureDisplay = GetComponent<Image>();
+
+         m_border.material = new Material(Shader.Find("Custom/DashedLine"));
+         m_border.material.SetColor("_MainColor", Color.grey);
+         m_border.startWidth = 0.05f;
+         m_border.endWidth = 0.05f;
+
+         m_border.useWorldSpace = false;
+         m_border.loop = true;
+
+         RectTransform l_trf = m_imgFurnitureDisplay.rectTransform;
+
+         Vector3[] l_corners = new Vector3[4] {
+            new Vector3(l_trf.rect.xMin, l_trf.rect.yMin, 0f),
+            new Vector3(l_trf.rect.xMin, l_trf.rect.yMax, 0f),
+            new Vector3(l_trf.rect.xMax, l_trf.rect.yMax, 0f),
+            new Vector3(l_trf.rect.xMax, l_trf.rect.yMin, 0f)
+         };
+
+         m_border.positionCount = l_corners.Length;
+         m_border.SetPositions(l_corners);
+      }
+   }
+}
