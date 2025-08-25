c2e8f986a29d43856c25900ee37adb73da7d7dcd
Mogador <jcaillaud17170@gmail.com>
2025-08-25 18:09:39 +0200

    Preparation for FurnitureUtils.GetProbabilityOfFalling

--- Assets/Scripts/Utils/FurnitureUtils.cs
+++ Assets/Scripts/Utils/FurnitureUtils.cs
@@ -1,39 +1,44 @@
-using System.Collections;
+using FFF.Interactable;
 using System.Collections.Generic;
 using UnityEngine;
+
+// TODO (A2) : Define Player class with int Stamina and int StaminaMax
+using Player = UnityEngine.MonoBehaviour;
+// TODO END
 
 public static class FurnitureUtils
 {
     // Get the probability of a Furniture
-    public static float GetProbabilityOfFalling(Player p_player, List<Furniture> p_lstFurniture)
+    public static float GetProbabilityOfFalling(Player p_player, List<DraggableFurnitureBehaviour> p_lstFurniture)
     {
         return GetProbabilityOfFallingV1(p_player, p_lstFurniture);
     }
 
     // Check if the cat succed to climb the furnitures
     // Return the index of the failing furniture, or -1 if succed
-    public static int TryClimbing(Player p_player, List<Furniture> p_lstFurniture)
+    public static int TryClimbing(Player p_player, List<DraggableFurnitureBehaviour> p_lstFurniture)
     {
         return TryClimbingV1(p_player, p_lstFurniture);
     }
 
     #region V1
 
-    private static float GetProbabilityOfFallingV1(Player p_player, List<Furniture> p_lstFurniture)
+    private static float GetProbabilityOfFallingV1(Player p_player, List<DraggableFurnitureBehaviour> p_lstFurniture)
     {
-        Furniture p_current = p_lstFurniture[0];
-        Furniture p_previous = p_lstFurniture[1];
+        Debug.Log("GetProbabilityOfFallingV1");
 
         /* ### RULE 1 ### */
         // You can't fall from the first furniture
-        if (p_previous == null) { return 0f; }
+        if (p_lstFurniture.Count < 2) { return 0f; }
+        
+        DraggableFurnitureBehaviour p_current = p_lstFurniture[0];
+        DraggableFurnitureBehaviour p_previous = p_lstFurniture[1];
 
-
-        int l_dCurrentStability = p_current.GetStability();
+        int l_dCurrentStability = p_current.CurrentStability;
 
         /* ### RULE 2 ### */
         // If the stability is bigger than the previous one, we subtract 1 from the current one
-        if (l_dCurrentStability > p_previous.GetStability())
+        if (l_dCurrentStability > p_previous.CurrentStability)
         {
             l_dCurrentStability--;
         }
@@ -45,25 +50,28 @@ public static class FurnitureUtils
 
         /* ### RULE 3 ### */
         // If player's stamina is lower than the stability (modified or not), we add stamina / staminaMax to the probability of falling
-        if (p_player.GetStamina() < l_dCurrentStability)
+
+        // TODO (A2) : Define Player class with int Stamina and int StaminaMax
+        /*if (p_player.Stamina < l_dCurrentStability)
         {
-            l_fProbabilityOfFalling += (float)p_player.GetStamina() / (float)p_player.GetStaminaMax();
-        }
+            l_fProbabilityOfFalling += (float)p_player.Stamina / (float)p_player.StaminaMax;
+        }*/
+        // TODO END
 
         /* ### RULE 4 ### */
         // If the (original) stability is equal to the previous one, we ignore the stability for the probability of falling
-        if (p_current.GetStability() == p_previous.GetStability()) { return l_fProbabilityOfFalling; }
+        if (p_current.CurrentStability == p_previous.CurrentStability) { return l_fProbabilityOfFalling; }
 
 
         /* ### RULE 5 ### */
         // If the stabilities are different we add 1 / stability to the probability of falling
         return l_fProbabilityOfFalling += 1f / l_dCurrentStability;
     }
-    private static int TryClimbingV1(Player p_player, List<Furniture> p_lstFurniture)
+    private static int TryClimbingV1(Player p_player, List<DraggableFurnitureBehaviour> p_lstFurniture)
     {
         for(int i = 0; i < p_lstFurniture.Count; i++)
         {
-            float l_fProbability = p_lstFurniture[i].GetProbability();
+            float l_fProbability = p_lstFurniture[i].CurrentProbability;
 
             // Maybe store the random value somewhere in the furniture for a visual effect ?
 
