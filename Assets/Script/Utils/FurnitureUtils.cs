faf8844e0cae69047b068310ecc73e6258203088
Mogador <jcaillaud17170@gmail.com>
2025-08-25 10:41:14 +0200

    FurnitureUtils : rename variables

--- Assets/Script/Utils/FurnitureUtils.cs
+++ Assets/Script/Utils/FurnitureUtils.cs
@@ -41,33 +41,33 @@ public static class FurnitureUtils
         // You fall if the stability is 0 or 1 (Dev's shortcut)
         if (l_dCurrentStability < 2) { return 1f; }
 
-        float l_probabilityOfFalling = 0f;
+        float l_fProbabilityOfFalling = 0f;
 
         /* ### RULE 3 ### */
         // If player's stamina is lower than the stability (modified or not), we add stamina / staminaMax to the probability of falling
         if (p_player.GetStamina() < l_dCurrentStability)
         {
-            l_probabilityOfFalling += (float)p_player.GetStamina() / (float)p_player.GetStaminaMax();
+            l_fProbabilityOfFalling += (float)p_player.GetStamina() / (float)p_player.GetStaminaMax();
         }
 
         /* ### RULE 4 ### */
         // If the (original) stability is equal to the previous one, we ignore the stability for the probability of falling
-        if (p_current.GetStability() == p_previous.GetStability()) { return l_probabilityOfFalling; }
+        if (p_current.GetStability() == p_previous.GetStability()) { return l_fProbabilityOfFalling; }
 
 
         /* ### RULE 5 ### */
         // If the stabilities are different we add 1 / stability to the probability of falling
-        return l_probabilityOfFalling += 1f / l_dCurrentStability;
+        return l_fProbabilityOfFalling += 1f / l_dCurrentStability;
     }
     private static int TryClimbingV1(Player p_player, List<Furniture> p_lstFurniture)
     {
         for(int i = 0; i < p_lstFurniture.Count; i++)
         {
-            float l_probability = p_lstFurniture[i].GetProbability();
+            float l_fProbability = p_lstFurniture[i].GetProbability();
 
             // Maybe store the random value somewhere in the furniture for a visual effect ?
 
-            if(Random.value <= l_probability)
+            if(Random.value <= l_fProbability)
             {
                 return i;
             }
