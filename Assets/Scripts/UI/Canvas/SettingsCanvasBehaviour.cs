047a37a8384835796ecb59dfba92309c79db9386
Lilian Sananikone <lilian.sananikone@laposte.net>
2025-08-29 18:51:25 +0200

    Add scripts for canvas switch

---
+++ Assets/Scripts/UI/Canvas/SettingsCanvasBehaviour.cs
@@ -0,0 +1,34 @@
+namespace FFF.Behaviours.UI
+{
+   public class SettingsCanvasBehaviour : CanvasBehaviour
+   {
+      //private SettingsSliderBehaviour[] m_sliders;
+      //
+      //private LanguageButtonBehaviour[] m_languageButtons;
+
+      public override void Reset()
+      {
+         //if(m_sliders == null)
+         //{
+         //   m_sliders = gameObject.GetComponentsInChildren<SettingsSliderBehaviour>();
+         //}
+         //
+         //foreach (SettingsSliderBehaviour l_slider in m_sliders)
+         //{
+         //   l_slider.Reset();
+         //}
+         //
+         //if (m_languageButtons == null)
+         //{
+         //   m_languageButtons = gameObject.GetComponentsInChildren<LanguageButtonBehaviour>();
+         //}
+         //
+         //foreach (LanguageButtonBehaviour l_button in m_languageButtons)
+         //{
+         //   l_button.Reset();
+         //}
+         //
+         base.Reset();
+      }
+   }
+}
